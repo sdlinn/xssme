c8d9843d820cfdccf3807012e2995359a84c59e8
Ginger <ginger@Gingers-MBP.wv.cc.cmu.edu>
2018-03-15 18:41:24 -0400

    added xmlhttprequest to content script

--- XSS Me/content.js
+++ XSS Me/content.js
@@ -2,12 +2,31 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if( request.message === "do_some_stuff" ) {
             alert("Got Message!");
-            // var stuff = null;
-            // if(stuff == null) {
-            //     sendResponse({did_stuff: "false"});
-            // } else {
-            //     sendResponse({did_stuff: "true"});
-            // }
+
+			var page_inputs = document.getElementsByTagName("input");
+
+			var num_inputs = page_inputs.length;
+			if (num_inputs > 0)
+			{
+				var data = new FormData();
+
+				for(var i = 0; i < num_inputs; i++)
+				{
+					if(page_inputs[i].type == 'text')
+						data.append(page_inputs[i].name, '<script>alert("xss detected")</script>');
+					else
+						data.append(page_inputs[i].name, page_inputs[i].value);
+				}
+				
+				var xhr = new XMLHttpRequest();
+				xhr.open('POST', window.location.href, true);
+
+				xhr.onload = function () {
+					//check results for vulnerabilities
+				};
+
+				xhr.send(data);
+			}
         }
     }
 );
